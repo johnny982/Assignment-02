03b06a47a397e22ff22645f79c33f60d4c1c7005
johnny982 <johnathan.donnelly@ontariotechu.net>
2024-11-03 11:57:41 -0500

    0.0.1

---
+++ README.md
@@ -0,0 +1,12 @@
+# Assignment-02
+
+## Versions
+
+* **0.0.1**
+    * Initial Commit
+    * Changed routes and footer
+    * Updated nav bar title, page titles, and icons
+
+
+## Credits
+* [Dot Symbol – Bullet Point in HTML Unicode](https://www.freecodecamp.org/news/dot-symbol-bullet-point-in-html-unicode/)
---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET index page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+module.exports = router;
